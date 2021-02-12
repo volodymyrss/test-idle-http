2b8851a6ff18abba4de306789efe850ab2a2df49
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-02-12 09:48:12 +0100

    simplified

--- client.sh
+++ client.sh
@@ -3,8 +3,9 @@ for i in $(seq 1 100); do
     for size_mb in 0.1 1.0 5.0 10.0 100.0; do
         echo -e '\n\033[31m===\033[0m'
 
+
         echo "$(date)" 
-        echo "$(curl -m 25 --connect-timeout 25 -X GET cdcicn01.isdc.unige.ch:5022/$size_mb/$sleep_s -o z 2>&1)"
+        curl -m 25 --connect-timeout 25 -X GET cdcicn01.isdc.unige.ch:5022/$size_mb/$sleep_s -o z 
         echo "$?" 
         echo "$(hostname)" 
         echo "$sleep_s" 
